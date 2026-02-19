be547023e98588bf9cc2fb98f103c0b6407c649e
koustubhdk34 <koustubhkulkarni34@gmail.com>
2026-02-19 21:40:08 +0530

    Change database settings

--- core/settings.py
+++ core/settings.py
@@ -92,12 +92,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # --------------------
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("POSTGRES_DB"),
-        'USER': os.getenv("POSTGRES_USER"),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-        'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT': os.getenv("POSTGRES_PORT", "5432"),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DB"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': os.getenv("MYSQL_PORT", "3306"),
     }
 }
 
